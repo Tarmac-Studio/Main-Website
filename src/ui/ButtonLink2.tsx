18b2a66711ba08f62c4d4fa36806ac4ffcf9f494
Olalere Ridwan Olamilekan <leksmannydev@gmail.com>
2025-09-27 21:55:12 +0100

    Button

--- src/ui/ButtonLink2.tsx
+++ src/ui/ButtonLink2.tsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 interface ButtonLinkProps {
   to: string;
+  target: string;
   children: React.ReactNode;
 }
 
-const ButtonLink2: React.FC<ButtonLinkProps> = ({ to, children }) => {
+const ButtonLink2: React.FC<ButtonLinkProps> = ({ to, target, children }) => {
   return (
     <Link
       to={to}
+      target={target}
       className="border-2 cyan-back text-black px-12 md:px-18 py-3 md:py-3 rounded-full text-base md:text-lg lg:text-xl font-medium text-center"
     >
       {children}
